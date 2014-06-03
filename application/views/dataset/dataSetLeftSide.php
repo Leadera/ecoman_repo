dc4dfe6a8b9acc6341159a5873fd181d91fc509e
etolan11 <etolan.11@gmail.com>
2014-06-03 10:22:27 +0300

    leftSide view eklendi

---
+++ application/views/dataset/dataSetLeftSide.php
@@ -0,0 +1,13 @@
+<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="form-group">
+					<ul class="nav nav-list">
+						<li><a style="text-transform:capitalize;" href="<?php echo base_url('new_flow'); ?>">Flows</a></li>
+						<li><a style="text-transform:capitalize;" href="<?php echo base_url('new_process'); ?>">Process</a></li>
+						<li><a style="text-transform:capitalize;" href="<?php echo base_url(''); ?>">Equipment</a></li>
+						<li><a style="text-transform:capitalize;" href="<?php echo base_url('new_product'); ?>">Product</a></li>
+						<li><a style="text-transform:capitalize;" href="<?php echo base_url(''); ?>">Component</a></li>
+					</ul>
+				</div>
+			</div>
