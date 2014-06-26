390aa2873e09ee240224ee5f20252c70a0cdf73d
Tuna Çağlar GÜMÜŞ <tunacaglargumus@gmail.com>
2014-06-26 13:49:55 +0300

    Anasayfa güncellendi

--- application/views/homepage/index.php
+++ application/views/homepage/index.php
@@ -1,3 +1,7 @@
 <div class="container">
-index
+<div class="lead">LEAD ERA Project: «ECOMANINDUSTRY»</div>
+	<p><img src="<?php echo asset_url('images/anasayfa.png'); ?>" alt="" class="img-responsive" style="border:1px solid #d0d0d0;"></p>
+	<p><b>Goal:</b> Increasing the overall eco-efficiency of industrial processes within a system of diverse factories</p>
+	<p><b>Approach:</b> Merging the concept of CP with IE in order to reduce the NPOs of the whole system by considering the NPOs from one industrial process as a potential useful input for another industrial process</p>
+	<p><b>Outcome:</b> Tool that consists of (1) independently from an industrial sector applicable software and (2) tailor-made modules for the manufacturing industries in Turkey and Switzerland</p>
 </div>
