301dc44c326f33a11cd52f629e34bd01f5f910c1
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2014-09-12 13:27:48 +0300

    show

--- application/views/cpscoping/show.php
+++ application/views/cpscoping/show.php
@@ -18,9 +18,10 @@
 				break;
 				}
 			}
+if($degisken == 1){
 
 				echo "<th>".$a['prcss_name']."</th>";
-			
+			}
 			?>
 			<?php endforeach ?>
 			</tr>
@@ -36,8 +37,9 @@
 					break;
 					}
 					}
-					
-			if ($a['flow_type_name'] == "Input"): ?>
+				if($degisken == 1): 	
+
+						if ($a['flow_type_name'] == "Input"): ?>
 			<tr>
 				<td rowspan="3"><?php echo $a['flow_name']; ?></td>
 				<td>Amount (<?php echo $a['unit_amount']; ?>)</td>
@@ -45,34 +47,55 @@
 					<?php 
 						$temp = "";
 							$temp = $a['flow_name'];
-							$tempp = $a['prcss_name'];
-							$sayi = 0;
-							for($j = 0 ; $j < count($allocation); $j++){
+							for($i = 0 ; $i < count($allocation); $i++){ 
+								$tempp = $allocation[$i]['prcss_name'];
+								$sayi = 0;
+								$prc_array = array(); $prccount = 0;
 
-								if($temp == $allocation[$j]['flow_name'] or $tempp == $allocation[$j]['prcss_name']){								
-									$sayi++;
-									echo "<td rowspan='3'>
-										".$sayi." / ".$allocation[$j]['prcss_name']." - ".$tempp." / ".$temp."
-										<table style='width:100%; font-size:13px;'>
-											<tr>
-												<td>".$allocation[$j]['amount']."</td><td rowspan='3'>test</td>
-											</tr>
-											<tr>
-												<td>".$allocation[$j]['cost']."</td>
-											</tr>
-											<tr>
-												<td>".$allocation[$j]['env_impact']."</td>
-											</tr>
-										</table>
-									</td>";
+								for($j = 0 ; $j < count($allocation); $j++){
+									$flow_name_temp = $allocation[$j]['flow_name'];
+									$prcss_name_temp = $allocation[$j]['prcss_name'];
+
+
+
+										$tuna = 1;
+										$prc_array[$prccount] = $prcss_name_temp;
+										//print_r($prc_array);
+										$prccount++;
+										for ($ip=0; $ip < $prccount-1; $ip++) { 
+											if($prc_array[$ip] == $prcss_name_temp){
+											$tuna = 0;
+											break;
+											}
+										}
+										if($tuna == 1){
+
+
+									if($temp == $flow_name_temp && $tempp == $prcss_name_temp){		
+
+										$sayi++;
+										echo "<td rowspan='3'>
+											".$sayi." / ".$allocation[$j]['prcss_name']." - ".$prcss_name_temp."
+											<table style='width:100%; font-size:13px;'>
+												<tr>
+													<td>".$allocation[$j]['amount']."</td><td rowspan='3'>test</td>
+												</tr>
+												<tr>
+													<td>".$allocation[$j]['cost']."</td>
+												</tr>
+												<tr>
+													<td>".$allocation[$j]['env_impact']."</td>
+												</tr>
+											</table>
+										</td>";
+									}
+									}
 								}
-								else{
-									echo "<td rowspan='3'>".$sayi."/".count($allocation)."</td>";
-								}
-								
+
+									if($sayi == 0){
+										echo "<td rowspan='3'>".$sayi."/".count($allocation)."</td>";
+									}
 							}
-
-							
 					
 					?>
 				</tr>
@@ -84,6 +107,8 @@
 					<td>Ep (UBP)</td>
 					<td><?php echo $a['env_impact']; ?></td>
 				</tr>
+						<?php endif ?>
+
 				<?php endif ?>
 				<?php endforeach ?>
 			</table>
