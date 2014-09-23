21febdfd6d07824cacd632e9872376729685f20b
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2014-09-23 10:27:04 +0300

    cp scoping index check

--- application/views/cpscoping/index.php
+++ application/views/cpscoping/index.php
@@ -26,41 +26,46 @@ $(document).ready(function() {
 </script>
 
 
-<div class="container">
-	<div class="row">
-		<div class="col-md-6">
-			<select id="projects" class="btn-group select select-block">
-				<option value="0">Nothing Selected</option>
-				<?php foreach ($c_projects as $p): ?>
-					<option value="<?php echo $p['proje_id']; ?>"><?php echo $p['name']; ?></option>
-				<?php	endforeach  ?>
-			</select>
-		</div>
-		<div class="col-md-6">
-			<select id="companiess" class="btn-group select select-block">
-				<option value="0">Nothing Selected</option>
-			</select>
-		</div>
-		<div class="col-md-12">
-			<a href="#" class="btn btn-default btn-sm" id="cpscopinga">New CP potentials identification</a>		
-		</div>
-        <div class="col-md-12">
-            <table class="table table-bordered">
-                <?php $i = 0; ?>
-                <?php foreach ($com_pro as $cp): ?>
-                    <tr>
-                        <th style="width:50%;" colspan="3">Company Name: <?php echo $cp['company_name']; ?> - Project Name: <?php echo $cp['project_name']; ?></th>
-                    </tr>                    
-                    <?php for($k = 0 ; $k < sizeof($flow_prcss[$i]) ; $k++): ?>
-                        <tr>
-                            <td>Used Process Name: <?php echo $flow_prcss[$i][$k]['prcss_name']; ?></td>
-                            <td>Used Flow Name: <?php echo $flow_prcss[$i][$k]['flow_name']; ?></td>
-                            <td><?php echo $flow_prcss[$i][$k]['flow_type_name']; ?></td>
-                        </tr>   
-                    <?php endfor ?>
-                    <?php $i++; ?>
-                <?php endforeach ?>
-            </table>
-        </div>
-	</div>
+<div class="col-md-3">
+    <p>Create new cp potentials identification</p>
+    <select id="projects" class="btn-group select select-block">
+        <option value="0">Nothing Selected</option>
+        <?php foreach ($c_projects as $p): ?>
+            <option value="<?php echo $p['proje_id']; ?>"><?php echo $p['name']; ?></option>
+        <?php   endforeach  ?>
+    </select>
+    <select id="companiess" class="btn-group select select-block">
+        <option value="0">Nothing Selected</option>
+    </select>
+    <a href="#" class="btn btn-default btn-sm" id="cpscopinga">New CP potentials identification</a>     
+</div>
+
+<div class="col-md-9">
+    <p>View and Edit Allocated CP Potentials Identifications</p>
+    <?php $i = 0; ?>
+    <?php foreach ($com_pro as $cp): ?>
+        <?php // print_r($cp); ?>
+        <div class="cp-heading">
+            <a href="<?php echo base_url('cpscoping/'.$cp['project_id'].'/'.$cp['company_id'].'/show'); ?>" class="btn btn-sm btn-info pull-right">View and Edit Cp Potentials Identifications</a>
+            <b>Company Name:</b> <?php echo $cp['company_name']; ?><br>
+            <b>Project Name:</b> <?php echo $cp['project_name']; ?>
+        </div>                    
+        <table class="table table-striped" style="font-size:12px;">
+        <?php if(sizeof($flow_prcss[$i])>0): ?>
+            <tr>
+                <th>Process Name</th>
+                <th>Flow Name</th>
+                <th>Flow Type</th>
+            </tr>
+        <?php endif ?>
+        <?php for($k = 0 ; $k < sizeof($flow_prcss[$i]) ; $k++): ?>
+            <tr>
+                <td><?php echo $flow_prcss[$i][$k]['prcss_name']; ?></td>
+                <td><?php echo $flow_prcss[$i][$k]['flow_name']; ?></td>
+                <td><?php echo $flow_prcss[$i][$k]['flow_type_name']; ?></td>
+            </tr>   
+        <?php endfor ?>
+        </table>
+        <?php $i++; ?>
+    <?php endforeach ?>
 </div>
