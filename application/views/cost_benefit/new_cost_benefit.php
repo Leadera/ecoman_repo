6b4a6d9c286305589fe0652a2a9ea7a24e185a5f
Burak Dikili <burakdikili@gmail.com>
2014-10-16 13:40:15 +0300

    cost_benefit düzeltme

--- application/views/cost_benefit/new_cost_benefit.php
+++ application/views/cost_benefit/new_cost_benefit.php
@@ -7,13 +7,15 @@
 			<th>Flow Name</th>
 			<th>Flow Type Name</th>
 		</tr>
-		<?php foreach ($cost_benefit as $co_benefit): ?>
-			<tr>
-				<td><?php echo $co_benefit['prcss_name']; ?></td>
-				<td><?php echo $co_benefit['flow_name']; ?></td>
-				<td><?php echo $co_benefit['flow_type_name']; ?></td>
-			</tr>
-		<?php endforeach ?>
+		<?php if (!empty($cost_benefit)): ?>
+			<?php foreach ($cost_benefit as $co_benefit): ?>
+				<tr>
+					<td><?php echo $co_benefit['prcss_name']; ?></td>
+					<td><?php echo $co_benefit['flow_name']; ?></td>
+					<td><?php echo $co_benefit['flow_type_name']; ?></td>
+				</tr>
+			<?php endforeach ?>
+		<?php endif ?>
 	</table>
 </div>
 <div class="col-md-4">
