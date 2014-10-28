8e73cf64b174b88735e07bb23074f984c0c04047
Burak Dikili <burakdikili@gmail.com>
2014-10-28 09:07:42 +0200

    git hub deneme

--- application/views/homepage/index.php
+++ application/views/homepage/index.php
@@ -5,4 +5,5 @@
 	<p><b>Approach:</b> Merging the concept of CP with IE in order to reduce the NPOs of the whole system by considering the NPOs from one industrial process as a potential useful input for another industrial process</p>
 	<p><b>Outcome:</b> Tool that consists of (1) independently from an industrial sector applicable software and (2) tailor-made modules for the manufacturing industries in Turkey and Switzerland</p>
 	<p>Deneme</p>
+	<p>Oldu</p>
 </div>
