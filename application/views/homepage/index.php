e89188625d961ef4d3609dea1515242a5c36e6df
Burak Dikili <burakdikili@gmail.com>
2014-10-28 10:40:34 +0200

    güncelleme

--- application/views/homepage/index.php
+++ application/views/homepage/index.php
@@ -4,5 +4,4 @@
 	<p><b>Goal:</b> Increasing the overall eco-efficiency of industrial processes within a system of diverse factories</p>
 	<p><b>Approach:</b> Merging the concept of CP with IE in order to reduce the NPOs of the whole system by considering the NPOs from one industrial process as a potential useful input for another industrial process</p>
 	<p><b>Outcome:</b> Tool that consists of (1) independently from an industrial sector applicable software and (2) tailor-made modules for the manufacturing industries in Turkey and Switzerland</p>
-	<p>Burak Dikili</p>
 </div>
