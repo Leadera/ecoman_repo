d6e452580f13b38640d2380b3ec8990bd33365d3
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2014-11-12 11:15:01 +0200

    navbar changes
    logo centered and navbar enlarged

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -23,65 +23,43 @@
   </head>
   <body>
 
-  <nav class="navbar navbar-default navbar-lg" role="navigation">
-  	<div class="container">
-  		<div class="row">
-        <div class="col-md-8">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
-            <span class="sr-only">Toggle navigation</span>
-          </button>
-          <a class="navbar-brand" href="<?php echo base_url(); ?>" style="color:white;">CPIS</a>
-          <div class="collapse navbar-collapse" id="navbar-collapse-01">
-            <ul class="nav navbar-nav">
-              <li><a href="<?php echo base_url('projects'); ?>">Projects</a></li>
-              <li><a href="<?php echo base_url('company'); ?>">Companies</a></li>
-              <li><a href="#fakelink">Forum</a></li>
-              <li><a href="#fakelink">Help</a></li>
-            </ul>
-          </div><!-- /.navbar-collapse -->
+    <nav class="navbar navbar-default navbar-lg" style="margin-bottom:0px;">
+      <a class="navbar-brand" href="<?php echo base_url(); ?>" style="color:white;">ECOMAN</a>
+      <ul class="nav navbar-nav navbar-left">
+        <li><a href="<?php echo base_url('projects'); ?>">Projects</a></li>
+        <li><a href="<?php echo base_url('company'); ?>">Companies</a></li>
+      </ul>
+      <form class="navbar-form navbar-left" action="<?php echo base_url('search') ?>" method="post" role="search">
+        <div class="form-group">
+          <div class="input-group">
+            <input name="term" class="form-control" id="navbarInput-01" type="search" placeholder="Search">
+            <span class="input-group-btn">
+              <button type="submit" class="btn"><span class="fui-search"></span></button>
+            </span>
+          </div>
         </div>
-        <div class="col-md-4">
-          <form class="navbar-form navbar-right" action="<?php echo base_url('search') ?>" method="post" role="search">
-            <div class="form-group">
-              <div class="input-group">
-                <input name="term" class="form-control" id="navbarInput-01" type="search" placeholder="Search">
-                <span class="input-group-btn">
-                  <button type="submit" class="btn"><span class="fui-search"></span></button>
-                </span>
-              </div>
-            </div>
-          </form>
-        </div>
-  		</div>
-
-  		<div class="row">
-  			<div class="col-md-9">
-          <?php
-            if ($this->session->userdata('user_in') !== FALSE):
-          ?>
-  				<ul class="list-inline">
-  				  <li class="head-li"><a href="<?php echo base_url('cpscoping'); ?>">CP Scoping</a></li>
-  				</ul>
-          <?php else: ?>
-            <p style="font-size:14px;">
-              To use the extended features of this web site, please register.
-            </p>
-          <?php endif ?>
-  			</div>
-  			<div class="col-md-3">
-  				<ul class="list-inline text-right">
-  					<?php
-  					  if ($this->session->userdata('user_in') !== FALSE):
-                $tmp = $this->session->userdata('user_in');
-    				?>
-              <li class="head-li"><a href="<?php echo base_url('user/'.$tmp['username']); ?>" style="text-transform: capitalize;"><?php echo $tmp['username']; ?></a></li>
-              <li class="head-li"><a href="<?php echo base_url('logout'); ?>">Log Out</a></li>
-            <?php else: ?>
-  					  <li class="head-li"><a href="<?php echo base_url('login'); ?>">Log In</a></li>
-  				  	<li class="head-li"><a href="<?php echo base_url('register'); ?>">Register</a></li>
-  					<?php endif ?>
-  				</ul>
-  			</div>
-  		</div>
-  	</div>
-  </nav><!-- /navbar -->
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <?php
+          if ($this->session->userdata('user_in') !== FALSE):
+            $tmp = $this->session->userdata('user_in');
+        ?>
+          <li class="head-li"><a href="<?php echo base_url('user/'.$tmp['username']); ?>" style="text-transform: capitalize;"><?php echo $tmp['username']; ?></a></li>
+          <li class="head-li"><a href="<?php echo base_url('logout'); ?>">Logout</a></li>
+        <?php else: ?>
+          <li class="head-li"><a href="<?php echo base_url('login'); ?>">Login</a></li>
+          <li class="head-li"><a href="<?php echo base_url('register'); ?>">Register</a></li>
+        <?php endif ?>
+      </ul>
+    </nav>
+    <div style="background-color: rgb(240, 240, 240); padding: 10px 20px; margin-bottom: 40px;">
+      <?php if ($this->session->userdata('user_in') !== FALSE): ?>
+        <ul class="list-inline" style="margin:0px;">
+          <li class="head-li"><a href="<?php echo base_url('cpscoping'); ?>">CP Scoping</a></li>
+        </ul>
+      <?php else: ?>
+        <p style="font-size:14px;">
+          To use the extended features of this web site, please register.
+        </p>
+      <?php endif ?>
+    </div>
