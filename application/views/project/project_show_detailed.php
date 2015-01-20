3723e5beeb4673fde6fca60d9d2009fe3f1a70cc
corner82 <311corner82@gmail.com>
2015-01-20 12:06:32 +0200

    project related companies map for indivigual project, related issue #103

--- application/views/project/project_show_detailed.php
+++ application/views/project/project_show_detailed.php
@@ -1,9 +1,20 @@
+<?php  
+    global $company_ids;
+    foreach ($companies as $company) {
+        $company_ids.= $company['name'].',';
+    }
+    $company_ids = rtrim($company_ids,',');
+?>
+<script>
+    console.log('<?php echo $company_ids;  ?>');
+</script>
 <div class="container">
 	<div class="row">
 		<div class="col-md-9">
 			<div class="lead pull-left"><?php echo $projects['name']; ?></div>
 			<?php if($is_consultant_of_project): ?>
 				<a class="btn btn-info btn-sm pull-right" href="<?php echo base_url("update_project/".$projects['id']); ?>">Update Project Info</a>
+                                <a onclick="event.preventDefault();window.open('../../IS_OpenLayers/map_prj.php?cmpny=<?php echo $company_ids; ?>','mywindow','width=900,height=900');" style = 'margin-right: 20px;' class="btn btn-info btn-sm pull-right" >See Project Companies On map</a>
 			<?php endif ?>
 			<table class="table table-bordered">
 				<tr>
