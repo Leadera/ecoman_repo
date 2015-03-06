73b5a014274ece91d409bb8739cd88af2dda8fca
corner82 <311corner82@gmail.com>
2015-03-07 01:56:15 +0200

    issue #181

--- assets/is/IS_js/scenarios.js
+++ assets/is/IS_js/scenarios.js
@@ -243,31 +243,87 @@
                 fit:true,
                 pagePosition : "both",
                 columns:[[
-                            {field:'prj_name',title:'IS Table Name',width:300},
+                            //{field:'prj_name',title:'IS Table Name',width:300,editor:'text'},
+                            {field:'prj_name',title:'IS Table Name',width:300,editor:{
+							type:'text',
+							options:{
+								required:true
+							}}
+                            },
                             {field:'syn_name',title:'Synergy Type',width:300},
                             {field:'date',title:' Project Date',width:300},
                             {field:'detail',title:' Details',width:100},
-                            {field:'action',title:'Action',width:150,align:'center',
-                                formatter:function(value,row,index){
-                                    if (row.editing){
-                                        var s = '<a href="#" onclick="saverow(this)">Save</a> ';
-                                        var c = '<a href="#" onclick="cancelrow(this)">Cancel</a>';
-                                        return s+c;
-                                    } else {
-                                         /*var d = '<button class="btn btn-mini rn_btnDelete" onclick="deleteISScenario(this)">Delete</button>';
-                                        return d;*/
-                                        var d = '<a href="#del" class="easyui-linkbutton" \n\
-                                            plain="true" iconCls="icon-cancel" \n\
-                                            onclick="/*event.prevenDefault();*/\n\
-                                                deleteISScenario(this);">Delete</a> ';
-                                        return d;
+                            {field:'action',title:'Action',width:80,align:'center',
+                                    formatter:function(value,row,index){
+                                            if (row.editing){
+                                                    var s = '<a href="javascript:void(0)" onclick="saverow(this)">Save</a> ';
+                                                    var c = '<a href="javascript:void(0)" onclick="cancelrow(this)">Cancel</a>';
+                                                    return s+c;
+                                            } else {
+                                                    var e = '<a href="javascript:void(0)" onclick="editrow(this)">Edit</a> ';
+                                                    //var d = '<a href="javascript:void(0)" onclick="deleteISScenario(this);">Delete</a>';
+                                                    var d = '<a href="javascript:void(0)" onclick="deleterow(this);">Delete</a>';
+                                                    return e+d;
+                                            }
                                     }
-                                }
-                            },
+                            }
                         ]],
-                singleSelect : true,
-                //closed:true,
-                //minimized:true,
+                        onBeforeEdit:function(index,row){
+					row.editing = true;
+					updateActions(index);
+				},
+				onAfterEdit:function(index,row){
+                                    console.log(row);
+                                    console.log('onAfterEdit');
+                                    if(row.prj_name==''){
+                                       $.messager.alert('Warning','Fill Scenario Name!','warning');
+                                    } 
+                                    else {
+                                        row.editing = false;
+                                        $.messager.confirm('Confirm','Are you sure? Scenario name will be updated...',function(r){
+                                            if (r){
+                                                
+                                                console.log(row);
+                                                $.ajax({
+                                                    url : '../../../Proxy/SlimProxy.php',   
+                                                    data : {
+                                                            url : 'updateScenario_scn',
+                                                            id : row.id,
+                                                            scenario : row.prj_name
+                                                    },
+                                                    type: 'GET',
+                                                    dataType : 'json',
+                                                    success: function(data, textStatus, jqXHR) {
+                                                        
+                                                        
+                                                        if(!data['notFound']) {
+                                                            if(data['id']>0) $.messager.alert('Scenario Updated','Updated succesfully!','info');
+                                                            //if(data['id']==0) $.messager.alert('Scenario Updated','Updated succesfully!','info');
+                                                        } else {
+                                                            $.messager.alert('Update Failure','Update failure!','error');
+                                                            /*console.warn('data notfound-->'+textStatus);
+                                                            $.messager.alert('Pick sub flow and company','Please select  a sub flow from flow tree!','warning');*/
+                                                        }
+                                                        $('#tt_grid_scenarios').datagrid('reload');
+                                                    },
+                                                    error: function(jqXHR , textStatus, errorThrown) {
+                                                      console.warn('error text status-->'+textStatus);
+                                                      $.messager.alert('Update Failure','Update failure!','error');
+                                                    }
+                                                });
+                                            }
+                                        });
+                                        //updateActions(index);
+                                    }	
+				},
+				onCancelEdit:function(index,row){
+				console.log('onCancelEdit');
+					row.editing = false;
+					updateActions(index);
+				},
+                        singleSelect : true,
+                        //closed:true,
+                        //minimized:true,
         });
         
         
@@ -322,6 +378,95 @@
 
 
 });  
+
+function updateActions(index){
+		console.log(index);
+                console.log('updateActions');
+		/*var row = $('#tt_grid_scenarios').datagrid('getSelected');
+                console.log('updateActions');
+		console.log(row);*/
+			$('#tt_grid_scenarios').datagrid('updateRow',{
+				index: index,
+				row:{}
+			});
+		}
+		function getRowIndex(target){
+			var tr = $(target).closest('tr.datagrid-row');
+			return parseInt(tr.attr('datagrid-row-index'));
+		}
+		function editrow(target){
+                console.log('editrow');
+		var rows = $('#tt_grid_scenarios').datagrid('getRows'); 
+		var row = rows[getRowIndex(target)];
+		console.log(row);
+		//$('#tt').datagrid('selectRow',getRowIndex(target));
+			$('#tt_grid_scenarios').datagrid('beginEdit', getRowIndex(target));
+			
+		}
+		function deleterow(target){
+			/*$.messager.confirm('Confirm','Are you sure?',function(r){
+				if (r){
+					$('#tt_grid_scenarios').datagrid('deleteRow', getRowIndex(target));
+				}
+			});*/
+                    $.messager.confirm('Confirm','Are you sure? Selected row will be deleted...',function(r){
+                        if (r){
+                            var rows = $('#tt_grid_scenarios').datagrid('getRows');
+                            var tr = $(target).closest('tr.datagrid-row');
+                            var rowIndex = parseInt(tr.attr('datagrid-row-index'));
+                            var row = rows[rowIndex];
+                            console.log(row);
+                            $.ajax({
+                                url : '../../../Proxy/SlimProxy.php',   
+                                data : {
+                                        url : 'deleteScenario_scn',
+                                        id : row.id
+                                },
+                                type: 'GET',
+                                dataType : 'json',
+                                success: function(data, textStatus, jqXHR) {
+                                    $('#tt_grid_scenarios').datagrid('reload');
+                                    if(!data['notFound']) {
+
+                                    } else {
+                                        /*console.warn('data notfound-->'+textStatus);
+                                        $.messager.alert('Pick sub flow and company','Please select  a sub flow from flow tree!','warning');*/
+                                    }
+                                },
+                                error: function(jqXHR , textStatus, errorThrown) {
+                                  console.warn('error text status-->'+textStatus);
+                                }
+                            });
+                        }
+                    });
+		}
+		function saverow(target){
+			
+			$('#tt_grid_scenarios').datagrid('endEdit', getRowIndex(target));
+			var rows = $('#tt_grid_scenarios').datagrid('getRows'); 
+			var row = rows[getRowIndex(target)];
+			console.log(row);
+			//console.error(getRowIndex(target));
+		}
+		function cancelrow(target){
+			$('#tt_grid_scenarios').datagrid('cancelEdit', getRowIndex(target));
+		}
+		function insert(){
+			var row = $('#tt_grid_scenarios').datagrid('getSelected');
+			if (row){
+				var index = $('#tt_grid_scenarios').datagrid('getRowIndex', row);
+			} else {
+				index = 0;
+			}
+			/*$('#tt_grid_scenarios').datagrid('insertRow', {
+				index: index,
+				row:{
+					status:'P'
+				}
+			});*/
+			$('#tt_grid_scenarios').datagrid('selectRow',index);
+			$('#tt_grid_scenarios').datagrid('beginEdit',index);
+		}
 
 
 
