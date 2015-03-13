63b0e79208f558c13f97ca79694b2e75871f6e67
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2015-03-13 16:49:50 +0200

    css change

--- application/views/homepage/index.php
+++ application/views/homepage/index.php
@@ -1,6 +1,6 @@
 <div class="homemain">
 	<div class="container text-center">
-		<div style="color: white;font-size: 28px;padding: 10px;margin-top: 80px;">License-free web-based support decision software for Cleaner Production and Industrial Symbiosis</div>
+		<div style="color: white;font-size: 28px;padding: 10px;margin-top: 60px;">License-free web-based support decision software <br> for Cleaner Production and Industrial Symbiosis</div>
 		<div style="margin-top:150px;">
 			<a class="btn btn-lg btn-success" href="<?php echo base_url('register'); ?>">Register</a>
 			<a class="btn btn-lg btn-success" href="<?php echo base_url('login'); ?>">Login</a>
