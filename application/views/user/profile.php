85271b6fec60129cb643abc854324dc3201ae4a9
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2015-05-15 12:07:22 +0200

    new user pages

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -1,5 +1,12 @@
 <div class="container">
 	<div class="row">
+		<div class="col-md-3">
+			<?php if(file_exists("assets/user_pictures/".$userInfo['photo'])): ?>
+					<img class="img-responsive thumbnail" src="<?php echo asset_url("user_pictures/".$userInfo['photo']); ?>">
+				<?php else: ?>
+					<img class="img-responsive thumbnail" src="<?php echo asset_url("user_pictures/default.jpg"); ?>">
+				<?php endif ?>
+		</div>
 		<div class="col-md-9">
 			<div style="overflow:hidden; margin-bottom: 10px;">
 				<?php  if($userInfo['id']==$this->session->userdata('user_in')['id']): ?>
@@ -13,7 +20,7 @@
 		  	<div class="label label-default">This user is a consultant</div>
 		  	<?php endif ?>
 		  </div>
-			<div class="lead" style="margin: 15px 0;"><?php echo $userInfo["name"].' '.$userInfo["surname"].' Profile'; ?></div>
+			<div class="swissheader" style="margin: 15px 0;"><?php echo $userInfo["name"].' '.$userInfo["surname"].' Profile'; ?></div>
 			<table class="table table-striped table-bordered">
 				<tr>
 					<td>
@@ -56,27 +63,27 @@
 					</td>
 				</tr>
 			</table>
+			<div class="row">
+				<div class="col-md-6">
+					<?php if($userInfo['role_id']==1): ?>
+					<div class="swissheader" style="font-size:15px;">Projects As a Consultant</div>
+					<ul class="nav nav-list">
+					<?php foreach ($projectsAsConsultant as $prj): ?>
+							<li><a style="text-transform:capitalize;" href="<?php echo base_url('project/'.$prj["proje_id"]) ?>"><?php echo $prj["name"] ?></a></li>
+					<?php endforeach ?>
+					</ul>
+					<?php endif ?>
+				</div>
+				<div class="col-md-6">
+					<div class="swissheader" style="font-size:15px;">Projects As a Contact Person</div>
+					<ul class="nav nav-list">
+						<?php foreach ($projectsAsWorker as $prj): ?>
+						<li><a style="text-transform:capitalize;" href="<?php echo base_url('project/'.$prj["proje_id"]) ?>"><?php echo $prj["name"] ?></a></li>
+						<?php endforeach ?>
+					</ul>
+				</div>
+			</div>
 		</div>
-		<div class="col-md-3">
-			<?php if(file_exists("assets/user_pictures/".$userInfo['photo'])): ?>
-				<img class="img-responsive thumbnail" src="<?php echo asset_url("user_pictures/".$userInfo['photo']); ?>">
-			<?php else: ?>
-				<img class="img-responsive thumbnail" src="<?php echo asset_url("user_pictures/default.jpg"); ?>">
-			<?php endif ?>
-			<ul class="nav nav-list">
-					<li class="nav-header" style="font-size:15px;">Projects As a Contact Person</li>
-			<?php foreach ($projectsAsWorker as $prj): ?>
-					<li><a style="text-transform:capitalize;" href="<?php echo base_url('project/'.$prj["proje_id"]) ?>"><?php echo $prj["name"] ?></a></li>
-			<?php endforeach ?>
-			</ul>
-			<?php if($userInfo['role_id']==1): ?>
-			<ul class="nav nav-list">
-					<li class="nav-header" style="font-size:15px;">Projects As a Consultant</li>
-			<?php foreach ($projectsAsConsultant as $prj): ?>
-					<li><a style="text-transform:capitalize;" href="<?php echo base_url('project/'.$prj["proje_id"]) ?>"><?php echo $prj["name"] ?></a></li>
-			<?php endforeach ?>
-			</ul>
-			<?php endif ?>
-		</div>
+
 	</div>
 </div>
