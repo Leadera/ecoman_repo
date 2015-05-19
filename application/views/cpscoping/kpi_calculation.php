d84315478b24cec59c0f0614ad3c5f016095bf15
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2015-05-19 13:04:55 +0200

    option coloring

--- application/views/cpscoping/kpi_calculation.php
+++ application/views/cpscoping/kpi_calculation.php
@@ -219,13 +219,13 @@
 				<thead>
 				    <tr>
 				        <th data-options="field:'allocation_name',align:'left',width:250">Allocation Name</th>
-<!-- 				        <th data-options="field:'flow_name',align:'center',width:110">Flow</th>
+<!-- 				    <th data-options="field:'flow_name',align:'center',width:110">Flow</th>
 				        <th data-options="field:'flow_type_name',align:'center',width:80">Flow Type</th> -->
 				        <th data-options="field:'kpi',align:'center',width:100">KPI</th>
 				        <th data-options="field:'benchmark_kpi',width:100,align:'center',editor:{type:'numberbox',options:{precision:5}}">Benchmark KPI</th>
 				        <th data-options="field:'unit_kpi',align:'center',width:100">KPI Unit</th>
 				        <th data-options="field:'best_practice',width:200,align:'center',editor:'text'">Comments / Remarks</th>
-				        <th data-options="field:'option',width:80,align:'center',editor:{type:'checkbox',options:{on:'Option',off:'Not An Option'}}">Is Option?</th>
+				        <th data-options="field:'option',width:80,align:'center',editor:{type:'checkbox',options:{on:'Option',off:'Not An Option'}}" formatter="formatOption">Is Option?</th>
 				        <th data-options="field:'allocation_id',width:100,align:'center'" formatter="formatDetail">Edit Allocation</th>
 				    </tr>
 				</thead>
@@ -241,6 +241,13 @@
 			function formatDetail(value,row){
 				var href = 'cpscoping/edit_allocation/'+value;
 				return '<a class="label label-info" href="<?php echo base_url("' + href + '"); ?>">Edit</a>';
+			}
+			function formatOption(val,row){
+			    if (val == "Option"){
+			        return '<span style="color:green;">('+val+')</span>';
+			    } else {
+			      	return '<span style="color:darkred;">('+val+')</span>';
+			    }
 			}
 		</script>
     <script type="text/javascript">
