2cb8b5edfcec63c2552afe5530751840a64c67c5
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2015-05-19 11:43:04 +0200

    KPI pages "edit allocation" button and rework

--- application/views/cpscoping/kpi_calculation.php
+++ application/views/cpscoping/kpi_calculation.php
@@ -217,14 +217,15 @@
 			    ">
 				<thead>
 				    <tr>
-				        <th data-options="field:'prcss_name',align:'center',width:100">Process</th>
-				        <th data-options="field:'flow_name',align:'center',width:110">Flow</th>
-				        <th data-options="field:'flow_type_name',align:'center',width:80">Flow Type</th>
+				        <th data-options="field:'allocation_name',align:'left',width:150">Allocation Name</th>
+<!-- 				        <th data-options="field:'flow_name',align:'center',width:110">Flow</th>
+				        <th data-options="field:'flow_type_name',align:'center',width:80">Flow Type</th> -->
 				        <th data-options="field:'kpi',align:'center',width:100">KPI</th>
-				        <th data-options="field:'unit_kpi',align:'center',width:100">KPI Unit</th>
 				        <th data-options="field:'benchmark_kpi',width:100,align:'center',editor:{type:'numberbox',options:{precision:5}}">Benchmark KPI</th>
+				        <th data-options="field:'unit_kpi',align:'center',width:100">KPI Unit</th>
 				        <th data-options="field:'best_practice',width:200,align:'center',editor:'text'">Comments / Remarks</th>
-				        <th data-options="field:'option',width:60,align:'center',editor:{type:'checkbox',options:{on:'Option',off:'Not An Option'}}">Is Option?</th>
+				        <th data-options="field:'option',width:80,align:'center',editor:{type:'checkbox',options:{on:'Option',off:'Not An Option'}}">Is Option?</th>
+				        <th data-options="field:'allocation_id',width:100,align:'center'" formatter="formatDetail">Edit Allocation</th>
 				    </tr>
 				</thead>
 			</table>
@@ -235,7 +236,12 @@
         <a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-undo',plain:true" onclick="reject()">Cancel All Changes</a>
         <a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-search',plain:true" onclick="getChanges()">See Changes</a>
     </div>
-    
+    <script>
+			function formatDetail(value,row){
+				var href = 'cpscoping/edit_allocation/'+value;
+				return '<a href="<?php echo base_url("' + href + '"); ?>">Edit</a>';
+			}
+		</script>
     <script type="text/javascript">
         var editIndex = undefined;
         function endEditing(){
