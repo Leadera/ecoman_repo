c584e6a81fa6786524174731487ecfecc115ad39
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2015-06-25 22:38:55 +0200

    contact person fix at update project

--- application/views/project/update_project.php
+++ application/views/project/update_project.php
@@ -100,3 +100,31 @@
     // Now let's align datepicker with the prepend button
     $(datepickerSelector).datepicker('widget').css({'margin-left': -$(datepickerSelector).prev('.btn').outerWidth()});
   </script>
+
+  <script type="text/javascript">
+  $(document).ready(function () {
+    $('#assignCompany').change(function () {
+      var company = $(this).val();
+      $.ajax({
+        url: "<?php echo base_url('contactperson');?>",
+        async: false,
+        type: "POST",
+        data: "company_id="+company,
+        dataType: "json",
+        success: function(data) {
+          //$('#assignContactPerson option').remove();
+
+          for (var k = 0; k < data.length; k++) {
+            for (var i = 0; i < data[k].length; i++) {
+              var opt =data[k][i]['id'];
+              if($("#assignContactPerson option[value='"+ opt +"']").length == 0)
+              {
+                $("#assignContactPerson").append(new Option(data[k][i]['name']+' '+data[k][i]['surname']+' - '+data[k][i]['cmpny_name'],data[k][i]['id']));
+              }
+            }
+          }
+        }
+      })
+    });
+  });
+</script>
