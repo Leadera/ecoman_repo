702f8d0813ee065e24e53a07328518a2f27c644f
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2015-06-25 17:30:30 +0200

    company name case sensitive control

--- application/controllers/company.php
+++ application/controllers/company.php
@@ -34,7 +34,7 @@ class Company extends CI_Controller{
 
 		$this->form_validation->set_rules('lat', 'Coordinates Latitude', 'trim|required|xss_clean');
 		$this->form_validation->set_rules('long', 'Coordinates Longitude', 'trim|required|xss_clean');
-		$this->form_validation->set_rules('companyName', 'Company Name', 'required|trim|xss_clean|is_unique[t_cmpny.name]');
+		$this->form_validation->set_rules('companyName', 'Company Name', 'required|trim|xss_clean|strtolower|is_unique[t_cmpny.name]');
 		$this->form_validation->set_rules('naceCode', 'Nace Code', 'required|trim|xss_clean');
 		$this->form_validation->set_rules('companyDescription', 'Company Description', 'required|trim|xss_clean');
 		$this->form_validation->set_rules('email', 'E-mail', 'required|trim|xss_clean');
