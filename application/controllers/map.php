99675021b6da0482a9b29891eb2f49b37c1b15e2
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2015-07-01 14:36:26 +0200

    Revert "sunucuda direk değiştirilen"
    This reverts commit df6f16e48d13794afdf1730c29c707ac0ea9564c.

--- application/controllers/map.php
+++ application/controllers/map.php
@@ -23,14 +23,14 @@ class Map extends CI_Controller {
                 redirect(base_url('projects'), 'refresh');
             }*/
             
-            /*if(isset($this->session->userdata['user_in']['role_id'])) {
+            if(isset($this->session->userdata['user_in']['role_id'])) {
                 if(($this->session->userdata['user_in']['role_id']==null || $this->session->userdata['user_in']['role_id']=='')
                          || $this->session->userdata['user_in']['role_id']!=1){
                        redirect(base_url('company'), 'refresh');
 		}
             } else {
-                //redirect(base_url('company'), 'refresh');
-            }*/
+                redirect(base_url('company'), 'refresh');
+            }
 
             $this->load->view('template/header_map');
             $this->load->view('map/index');
@@ -42,27 +42,27 @@ class Map extends CI_Controller {
             if(isset($this->session->userdata['user_in'])) {
                if(empty($this->session->userdata['user_in'])){
 			redirect(base_url('login'),'refresh');
-				} 
+		} 
             } else {
                 redirect(base_url('login'),'refresh');
             }
                 
-            /*if(isset($this->session->userdata['project_id'])) {
+            if(isset($this->session->userdata['project_id'])) {
                 if($this->session->userdata['project_id']==null || $this->session->userdata['project_id']==''){
                     redirect(base_url('projects'), 'refresh');
                 }
             } else {
                 redirect(base_url('projects'), 'refresh');
-            }*/
+            }
             
-            /*if(isset($this->session->userdata['user_in']['role_id'])) {
+            if(isset($this->session->userdata['user_in']['role_id'])) {
                 if(($this->session->userdata['user_in']['role_id']==null || $this->session->userdata['user_in']['role_id']=='')
                          || $this->session->userdata['user_in']['role_id']!=1){
                        redirect(base_url('company'), 'refresh');
-				}
+		}
             } else {
-                //redirect(base_url('company'), 'refresh');
-            }*/
+                redirect(base_url('company'), 'refresh');
+            }
 
             $this->load->view('map/mapHeader');
            
