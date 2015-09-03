e3069e8670c54d86e113acebd8fd690267920e2d
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2015-09-03 13:14:53 +0300

    error pages

--- application/errors/error_general.php
+++ application/errors/error_general.php
@@ -1,62 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Error</title>
-<style type="text/css">
+  <meta charset="utf-8">
+  <title><?php echo lang("slogan"); ?></title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <!-- Loading Bootstrap -->
+  <link href="<?php echo asset_url('bootstrap/css/bootstrap.css'); ?>" rel="stylesheet">
 
-::selection{ background-color: #E13300; color: white; }
-::moz-selection{ background-color: #E13300; color: white; }
-::webkit-selection{ background-color: #E13300; color: white; }
-
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	-webkit-box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+  <!-- Loading Flat UI -->
+  <link href="<?php echo asset_url('css/flat-ui.css'); ?>" rel="stylesheet">
+  <link href="<?php echo asset_url('css/custom.css'); ?>" rel="stylesheet">
+  <link href="<?php echo asset_url('css/selectize.css'); ?>" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo asset_url('css/font-awesome.min.css'); ?>">
+  <script src="<?php echo asset_url('js/jquery-1.10.2.min.js'); ?>"></script>
+  <script src="<?php echo asset_url('js/bootstrap.min.js'); ?>"></script>
+  <style type="text/css">
+		body{
+			background-color: #2d6b6a;
+		}
+  </style>
 </head>
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+	<div style="text-align: center;">
+		<a style="color:white; font-weight: bold; font-size:30px; line-height: 12px; padding: 20px; padding-bottom: 10px; margin-top:40px; display: block;"  href="<?php echo base_url(); ?>" style="color:white;">
+			ECOMAN<span style="color:#f0f0f0; font-size:12px;"><br>Go homepage</span>
+		</a>
+	</div>
+	<div class="container" style="margin-top: 40px; margin-bottom: -50px;">
+		<div class="col-md-8 col-md-offset-2" style="padding: 20px;">
+		<h6 style="margin: 0px;
+font-size: 30px;
+font-weight: 200;
+line-height: 3;
+text-shadow: 0 1px 2px rgba(0,0,0,.2); color:white; text-align: center;"><?php echo $heading; ?> :(</h6>
+		<pre style="color:white; background:none;"><?php echo $message; ?></pre>
+		</div>
 	</div>
 </body>
 </html>
--- application/errors/error_php.php
+++ application/errors/error_php.php
@@ -1,10 +1,44 @@
-<div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title><?php echo lang("slogan"); ?></title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <!-- Loading Bootstrap -->
+  <link href="<?php echo asset_url('bootstrap/css/bootstrap.css'); ?>" rel="stylesheet">
 
-<h4>A PHP Error was encountered</h4>
-
-<p>Severity: <?php echo $severity; ?></p>
-<p>Message:  <?php echo $message; ?></p>
-<p>Filename: <?php echo $filepath; ?></p>
-<p>Line Number: <?php echo $line; ?></p>
-
-</div>
+  <!-- Loading Flat UI -->
+  <link href="<?php echo asset_url('css/flat-ui.css'); ?>" rel="stylesheet">
+  <link href="<?php echo asset_url('css/custom.css'); ?>" rel="stylesheet">
+  <link href="<?php echo asset_url('css/selectize.css'); ?>" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo asset_url('css/font-awesome.min.css'); ?>">
+  <script src="<?php echo asset_url('js/jquery-1.10.2.min.js'); ?>"></script>
+  <script src="<?php echo asset_url('js/bootstrap.min.js'); ?>"></script>
+  <style type="text/css">
+		body{
+			background-color: #2d6b6a;
+		}
+  </style>
+</head>
+<body>
+	<div style="text-align: center;">
+		<a style="color:white; font-weight: bold; font-size:30px; line-height: 12px; padding: 20px; padding-bottom: 10px; margin-top:40px; display: block;"  href="<?php echo base_url(); ?>" style="color:white;">
+			ECOMAN<span style="color:#f0f0f0; font-size:12px;"><br>Go homepage</span>
+		</a>
+	</div>
+	<div class="container" style="margin-top: 40px; margin-bottom: -50px;">
+		<div class="col-md-8 col-md-offset-2" style="padding: 20px;">
+		<h6 style="margin: 0px; font-size: 30px; font-weight: 200; line-height: 3; text-shadow: 0 1px 2px rgba(0,0,0,.2); color:white; text-align: center;">
+			A PHP Error was encountered :(
+		</h6>
+		<h4></h4>
+		<pre style="color:white; background:none;">
+			<p>Severity: <?php echo $severity; ?></p>
+			<p>Message:  <?php echo $message; ?></p>
+			<p>Filename: <?php echo $filepath; ?></p>
+			<p>Line Number: <?php echo $line; ?></p>
+		</pre>
+		</div>
+	</div>
+</body>
+</html>
