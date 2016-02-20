98646581cc2d08d4822efaed9c218d118c003981
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2016-02-20 14:56:17 +0200

    delete

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -49,15 +49,6 @@
         });
       </script>
     <?php endif ?>
-    <!--Start of Zopim Live Chat Script-->
-<script type="text/javascript">
-window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
-d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
-$.src="//v2.zopim.com/?3hv5ibobzffc4gKWuMFyk0qD59a0FSc8";z.t=+new Date;$.
-type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
-</script>
-<!--End of Zopim Live Chat Script-->
   </head>
   <body <?php /*if($this->uri->segment(1)=="isscoping" or $this->uri->segment(1)=="isscopingauto"){echo 'class="easyui-layout"';}*/ ?>>
 
