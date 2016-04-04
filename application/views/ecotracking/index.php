2611e0fb25369a80e9c4dc12f0113ef7dcc5b476
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2016-04-04 20:49:44 +0200

    eco tracking

--- application/views/ecotracking/index.php
+++ application/views/ecotracking/index.php
@@ -1,5 +1,6 @@
 	<div class="col-md-4 col-md-offset-4">
 		<p><?php echo lang("ecoheading"); ?></p>
-		<a href="<?php echo base_url('ecotracking/131/1'); ?>" class="btn btn-inverse">Dizayn Makina</a>
+		<a href="<?php echo base_url('ecotracking/132/1'); ?>" class="btn btn-inverse">Dizayn Makina A.Ş.</a>
+		<a href="<?php echo base_url('ecotracking/131/1'); ?>" class="btn btn-inverse">TOBB ETU laboratory</a>
 	</div>
 
